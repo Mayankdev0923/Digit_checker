20771fd85e5a5caf6d216667210ef01cd3a17d21
Mayankdev0923 <143903748+Mayankdev0923@users.noreply.github.com>
2023-09-05 05:09:54 +0530

    Added the main C code.

---
+++ digitchecker.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  printf("Enter your character : ");
+  
+  char ch;
+  scanf("%c",&ch);
+ 
+  
+  if ( ch == '1' ) { 
+    printf("\nThe character entered is a digit");
+  }
+  else if ( ch == '2' ) { 
+    printf("\nThe character entered is a digit");
+  }
+  else if ( ch == '3' ) { 
+    printf("\nThe character entered is a digit");
+  }
+  else if ( ch == '4' ) { 
+    printf("\nThe character entered is a digit");
+  }
+  else if ( ch == '5' ) { 
+    printf("\nThe character entered is a digit");
+  }
+  else if ( ch == '6' ) { 
+    printf("\nThe character entered is a digit");
+  }
+  else if ( ch == '7' ) { 
+    printf("\nThe character entered is a digit");
+  }
+  else if ( ch == '8' ) { 
+    printf("\nThe character entered is a digit");
+  }
+  else if ( ch == '9' ) { 
+    printf("\nThe character entered is a digit");
+  }
+  else if ( ch == '0' ) { 
+    printf("\nThe character entered is a digit");
+  }
+  
+ else {
+    printf("\nThe character entered is not a digit");
+  }
+  
+  printf("\n--------------------------------------------------\n\n");
+  
+  
+  
+  return 0;
+}
